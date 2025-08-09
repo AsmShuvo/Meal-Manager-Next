8f21f18099ca5a773169c2610257d116f5efc321
AsmShuvo <hoffmanhulk99@gmail.com>
2025-08-09 23:46:13 +0600

    Add: Next Auth with credentials

---
+++ src/app/Components/Navbar.jsx
@@ -0,0 +1,35 @@
+"use client"; // if you want interactivity (clicks, routing)
+
+import Link from "next/link";
+import { useSession, signIn, signOut } from "next-auth/react"
+
+const links = [
+  { label: "Home", href: "/" },
+  { label: "About", href: "/about" },
+  { label: "Services", href: "/services" },
+  { label: "Contact", href: "/contact" },
+];
+
+export default function Navbar() {
+  return (
+    <nav style={{ padding: "1rem", borderBottom: "1px solid #ccc" }}>
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <div>
+          <strong>MyApp</strong>
+        </div>
+
+        <ul style={{ display: "flex", listStyle: "none", gap: "1rem", margin: 0, padding: 0 }}>
+          {links.map((link) => (
+            <li key={link.href}>
+              <Link href={link.href}>{link.label}</Link>
+            </li>
+          ))}
+        </ul>
+
+        <div>
+          <button onClick={()=>signIn()} >Login</button>
+        </div>
+      </div>
+    </nav>
+  );
+}
---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,26 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const handler = NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "John Doe" },
+        email: { label: "Email", type: "text", placeholder: "zyx@gmail.com" },
+        password: { label: "Password", type: "email" },
+      },
+      async authorize(credentials, req) {
+        const user = { id: "1", name: "J Smith", email: "jsmith@example.com" };
+
+        if (user) {
+          return user;
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
+});
+
+export { handler as GET, handler as POST };
